2ea6785032accf35e96b3e1b2887966f3fd2c815
Maksim Zhuikov <sinplym1@gmail.com>
2024-03-08 18:58:04 +0500

    fix: import

--- src/components/WorkListFilter/index.tsx
+++ src/components/WorkListFilter/index.tsx
@@ -1,4 +1,4 @@
-import {Dispatch, FC, SetStateAction, useState} from "react";
+import {Dispatch, FC, SetStateAction} from "react";
 
 import Box from "@mui/material/Box";
 import {DatePicker} from "@mui/x-date-pickers";
